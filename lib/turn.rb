f340c4a275823945e19da55abd3364d2852f1eef
David Medina <davidamedina619@gmail.com>
2020-09-11 20:30:09 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-
+def input_to_index(user_input) 
+ input_to_index=user_input.to_i - 1
+end
 
 
 def valid_move?(board, index)
@@ -30,9 +32,7 @@ end
 
 
 
-def input_to_index(user_input) 
- input_to_index=user_input.to_i - 1
-end
+
 
 def move(board,index,player="X")
   board[index] = player
