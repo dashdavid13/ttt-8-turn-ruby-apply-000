9789f881db60f41763b2791ac7968518027f3c7f
David Medina <davidamedina619@gmail.com>
2020-09-11 20:29:57 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,14 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+
+
 def valid_move?(board, index)
    if index.between?(0,8) && !position_taken?(board, index)
       return true
@@ -17,13 +28,7 @@ def position_taken?(board, index)
 end
 
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+
 
 def input_to_index(user_input) 
  input_to_index=user_input.to_i - 1
