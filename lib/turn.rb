d769f24415a438f85224b336a42254693fcb74f4
David Medina <davidamedina619@gmail.com>
2020-09-11 20:09:15 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,5 +37,10 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
+  index = input_to_index(user_input)
+   if valid_move?(board, index)
+      move(board, index, current_player(board))
+      turn(board)
+   end
 end
 
