cdee7c26bfe5a5353e82781e88f0a53c6d0518f7
David Medina <davidamedina619@gmail.com>
2020-09-11 20:04:03 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,11 +36,6 @@ def move(board,index,player="X")
 end
 
 def turn(board)
-  index = input_to_index(user_input)
-   if valid_move?(board, index)
-      move(board, index, current_player(board))
-      turn(board)
-   end
-   display_board(board)
+  
 end
 
